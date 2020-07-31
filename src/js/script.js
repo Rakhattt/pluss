accfc48f17c89f5782c9427d2c61e097f2645b7d
Rakhattt <67402354+Rakhattt@users.noreply.github.com>
2020-07-31 20:01:13 +0600

    init

---
+++ src/js/script.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/left_arrow.png"></button>',
+        nextArrow:'<button type="button" class="slick-next"><img src="img/right_arrow.png"></button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                  dots: true,
+                  arrow: false
+                }  
+            }
+        ]
+      });
+
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+ 
+     
+     function toggleSlide(item){
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                 e.prenentDefault();
+                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                 $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+         }); 
+     };
+
+      toggleSlide('.catalog-item__link');
+      toggleSlide('.catalog-item__back');
+
+
+    $('[data-modal=consultation]').on('click', function() {
+      $('.ovelay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+      $('.overlay, #consultation, #thanks, #order').fadeOut('slow')
+    });  
+    $('.button_mini').on('click', function() {
+      $('.overlay, #order').fadeIn('slow')
+    }); 
+  });
